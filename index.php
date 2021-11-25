cdf76453044b129450ce3b76a0f49483ea746780
mvillanuevas <mvillanueva.1310@gmail.com>
2021-11-25 11:12:28 -0800

    .

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ use src\VerifyXmlRequest as verifica;
 use src\Utils as util;
 
 
-$cert = file_get_contents('resources/cer.cer');
-$key = file_get_contents('resources/key.key');
-$rfc = 'LAN7008173R5';
-$fechaInicial = '2018-06-02T00:00:00';
-$fechaFinal = '2018-06-02T12:59:59';
+$cert = file_get_contents('resources/fes130828my0.cer');
+$key = file_get_contents('resources/Claveprivada_FIEL_FES130828MY0_20211118_092622.key');
+$rfc = 'FES130828MY0';
+$fechaInicial = '2021-10-01T00:00:00';
+$fechaFinal = '2021-10-31T12:59:59';
 $TipoSolicitud = 'CFDI';
 $idSolicitud = '1fb832ff-6a25-4616-8ca8-04478690cc29';
 $idPaquete = '1fb832ff-6a25-4616-8ca8-04478690cc29_01';
